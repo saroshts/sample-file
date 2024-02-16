f2cdbd1e0ecdd6701519330b1a2f1921a458fdd9
saroshts <160051914+saroshts@users.noreply.github.com>
2024-02-16 23:54:25 +0530

    Update Hello_world.py

--- Hello_world.py
+++ Hello_world.py
@@ -1,8 +1,18 @@
-#!/usr/bin/env python
-# coding: utf-8
+import os
+import mysql.connector
 
-# In[1]:
+mydb = mysql.connector.connect(
+host= '18.136.157.135',
+user= 'dm_team10',
+password= 'DM!$!Team!92@05!156&',
+port= '3306',
+database= 'project_movie_database'
+)
 
+sql= "select * from directors"
+mycursor = mydb.cursor()
+mycursor.execute(sql)
+myresult = mycursor.fetchall()
+print (myresult) 
 
-print("Hello Python")
 
